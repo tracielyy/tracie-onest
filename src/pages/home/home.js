ffb36392e6d58893b00197d9bd394d6a2459ff95
Tracie <57853991+tracieqwynn@users.noreply.github.com>
2021-08-27 09:44:49 +0800

    add link and improve the code

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -7,19 +7,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from "../../components/FragContainer"
 import { ArrowRight } from "react-bootstrap-icons";
 
-const Service = ({ eKey, header, content }) => {
-    var content_txt = [];
-    for (let i in content) {
-        content_txt.push(<div><ArrowRight /> {content[i]}</div>);
-    }
+const Service = ({ eKey, header, contents }) => {
 
     return (
         <Accordion.Item eventKey={eKey}>
             <Accordion.Header>{header}</Accordion.Header>
             <Accordion.Body className="text-left" >
-                <div>
-                    {content_txt}
-                </div>
+                <>
+                    {contents.map(content => (
+                        <div className="content"><ArrowRight /> {content.main}
+                            {content.url ? <a href={content.url}>here</a> : null}.
+                        </div>
+                    ))}
+                </>
             </Accordion.Body>
         </Accordion.Item >
     );
@@ -28,14 +28,18 @@ const Service = ({ eKey, header, content }) => {
 const Home = () => {
 
     // Service 1
-    let service1_content = [];
-    service1_content.push("Verify standard identification number of an entity.");
-    service1_content.push("Make sure that the UEN provided comply with the correct format.");
+    let service1_content = [
+        { main: 'Verify standard identification number of an entity.' },
+        { main: 'Make sure that the UEN provided comply with the correct format.' },
+        { main: 'There are valid formats: (A) Business registered with ACRA, (B) Local companies with ACRA, (C) All other entities with new UEN' },
+        { main: 'For more information please refer to ', url: 'https://www.uen.gov.sg/ueninternet/faces/pages/admin/aboutUEN.jspx' }
+    ];
 
     // Service 2
-    let service2_content = [];
-    service2_content.push('View Singapore weather forecast for the next 2 hours.');
-    service2_content.push('Users is able to choose a valid location in Singapore to know its weather forecast.');
+    let service2_content = [
+        {main:'View Singapore weather forecast for the next 2 hours.'},
+        {main: 'Users is able to choose a valid location in Singapore to know its weather forecast.'}
+    ];
 
 
     return (
@@ -47,9 +51,9 @@ const Home = () => {
                         <Card.Title>Welcome to Tracie's OneST</Card.Title>
                         <Accordion >
                             <Service eKey="0" header="Service 1: UEN Validate"
-                                content={service1_content} />
+                                contents={service1_content} />
                             <Service eKey="1" header="Service 2: Weather Forecast"
-                                content={service2_content} />
+                                contents={service2_content} />
                         </Accordion>
                     </Card.Body>
                 </Card>
